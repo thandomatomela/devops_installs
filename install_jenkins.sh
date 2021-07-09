e872373c20ba02c4cf7c07ef3190397300464d37
thandomatomela <42520803+thandomatomela@users.noreply.github.com>
2021-07-09 22:00:42 +0200

    Create install_jenkins.sh

---
+++ install_jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update
+sudo apt search openjdk
+apt install -y openjdk-11-jre-headless
+java -version
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install -y jenkins
