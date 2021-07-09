1d8b2a82f19bf78f004530089bff2643a60be3e0
thandomatomela <42520803+thandomatomela@users.noreply.github.com>
2021-07-09 22:02:31 +0200

    Create install_maven.sh

---
+++ install_maven.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /opt
+wget https://downloads.apache.org/maven/maven-3/3.8.1/binaries/apache-maven-3.8.1-bin.tar.gz
+tar -xvzf apache-maven-3.8.1-bin.tar.gz
+mv apache-maven-3.8.1 maven
+cd maven
+
+echo "M2_HOME=/opt/maven" >> ~/.bash_profile
+echo "M2=/opt/maven/bin" >> ~/.bash_profile
+echo "PATH=$PATH:$HOME/bin:$M2:$M2_HOME" >> ~/.bash_profile
+echo "export PATH" >> ~/.bash_profile
+mvn --version 
