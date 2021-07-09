295ad1a5732a3393ffb3951da11ddab30af83192
thandomatomela <42520803+thandomatomela@users.noreply.github.com>
2021-07-09 21:57:55 +0200

    Create master_docker.sh

---
+++ master_docker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+touch ~/.vimrc
+echo "set nocompatible" > ~/.vimrc
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get -y update
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+
