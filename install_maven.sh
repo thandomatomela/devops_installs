afce58f473d21c61b1dc73026851d1ab3fc294ba
thandomatomela <42520803+thandomatomela@users.noreply.github.com>
2021-07-11 17:12:14 +0200

    Update install_maven.sh

--- install_maven.sh
+++ install_maven.sh
@@ -5,6 +5,7 @@ wget https://downloads.apache.org/maven/maven-3/3.8.1/binaries/apache-maven-3.8.
 tar -xvzf apache-maven-3.8.1-bin.tar.gz
 mv apache-maven-3.8.1 maven
 cd maven
+apr-get install maven
 
 echo "M2_HOME=/opt/maven" >> ~/.bash_profile
 echo "M2=/opt/maven/bin" >> ~/.bash_profile
